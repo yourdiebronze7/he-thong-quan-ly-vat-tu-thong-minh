2ce1e458906f57dd1951a2e43d181a0a44fdbad5
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-02 15:55:09 +0000

    Update port number to 4000 for development environment.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const itemRoutes = require('./routes/itemRoutes');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());
 app.use('/api/items', itemRoutes);
